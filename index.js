76ecb37929b40fbf458adf71324d8ea74b56ba69
unknown <avilasonson@gmail.com>
2023-04-21 13:05:32 +0800

    parsed successfully

---
+++ index.js
@@ -0,0 +1,6 @@
+/*fetch("file:///C:/Users/Dell/Desktop/xmljsonparser/xml/data.xml").then(response => {
+    return response.text();
+}).then(xmlString =>{
+    const xmlDocument = new DOMParser().parseFromString(xmlString, "text/xml");
+    console.log(xmlDocument);
+})*/
